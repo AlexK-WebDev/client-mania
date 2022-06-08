3b1ee17f71ff2ef7074b7b7a329cd6ea20a927d2
scrollcode4us <scrollcode.4.us@gmail.com>
2022-06-08 18:57:38 +0300

    git local init

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+
+	var $toggleButton = $('.toggle-button'),
+    	$menuWrap = $('.menu-wrap'),
+    	$sidebarArrow = $('.sidebar-menu-arrow');
+
+	// Hamburger button
+
+	$toggleButton.on('click', function() {
+		$(this).toggleClass('button-open');
+		$menuWrap.toggleClass('menu-show');
+	});
+
+	// Прокрутка 
+	$('.go-to').click(function (e) {
+		e.preventDefault();
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$("body,html").animate({scrollTop: destination }, 1200);
+		});
+
+	$('.carousel').carousel({
+	  interval: false
+	})
+
+
+});
